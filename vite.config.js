a1a42e094eed886bf76af4f2bec7212ec1126fbd
NathanPrice <npricephoto@gmail.com>
2024-02-20 11:38:17 -0500

    fixed everything I broke to make my static site work

--- vite.config.js
+++ vite.config.js
@@ -6,25 +6,5 @@ import libCss from 'vite-plugin-libcss';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), libCss()],
-  build: {
-    // lib: {
-    //   // Could also be a dictionary or array of multiple entry points
-    //   entry: resolve(__dirname, 'lib/main.js'),
-    //   name: 'DiceRoller',
-    //   // the proper extensions will be added
-    //   fileName: 'dice-roller',
-    // },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ['vue', 'tailwindcss'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: 'Vue',
-        },
-      },
-    },
-  },
+  base: './',
 })
