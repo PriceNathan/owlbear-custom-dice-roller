4b938cc75211b7bb5bc41cb0c6535f2675b97c40
NathanPrice <npricephoto@gmail.com>
2024-02-20 11:18:40 -0500

    trying something new and building as static site

--- vite.config.js
+++ vite.config.js
@@ -7,13 +7,13 @@ import libCss from 'vite-plugin-libcss';
 export default defineConfig({
   plugins: [vue(), libCss()],
   build: {
-    lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'lib/main.js'),
-      name: 'DiceRoller',
-      // the proper extensions will be added
-      fileName: 'dice-roller',
-    },
+    // lib: {
+    //   // Could also be a dictionary or array of multiple entry points
+    //   entry: resolve(__dirname, 'lib/main.js'),
+    //   name: 'DiceRoller',
+    //   // the proper extensions will be added
+    //   fileName: 'dice-roller',
+    // },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
